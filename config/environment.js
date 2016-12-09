309e9434fcb54f7591e372c18526c0df0bd275aa
unknown <i94414@IPVAPW7D-A105.iso.com>
2016-12-09 15:24:51 +0530

    removed proxy settings

--- config/environment.js
+++ config/environment.js
@@ -26,6 +26,14 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
+  
+  if (environment === 'production') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
 
   if (environment === 'test') {
     // Testem prefers this...
